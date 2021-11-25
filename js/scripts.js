100792f637c0ca75b62f5b2d2deab3207564877a
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2021-11-25 14:27:40 +0100

    Created my 3 first pokemon objects
    Snorlax, mewtwo and shiftry were created.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,2 @@
-
+let pokemonList = [{name: 'snorlax', height: 2.1, types: ["normal", "water"]}, {name:'mewtwo', height: 2, types: ['psychic', 'flying'] },
+{name:'shiftry', height: 1.3, types: ['dark', 'grass'] }];
