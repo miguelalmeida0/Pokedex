33e283e59f7bca7cfc2a264e0f477808ee436c71
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2021-11-25 14:32:50 +0100

    My first 3 Pokemon objects but indented!

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
   let pokemonList = [{name: 'snorlax', height: 2.1, types: ["normal", "water"]},
   {name:'mewtwo', height: 2, types: ['psychic', 'flying'] },
   {name:'shiftry', height: 1.3, types: ['dark', 'grass'] }];
-
-console.log(pokemonList)
