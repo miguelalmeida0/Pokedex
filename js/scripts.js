c9f6caa7b670181063b11a1a673f15cbe0eaa19b
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2021-11-26 17:45:50 +0100

    had double quotes and single quotes in the same file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 
-  let pokemonList = [{name: 'snorlax', height: 2.1, types: ["normal", "water"]},
-  {name:'mewtwo', height: 2, types: ['psychic', 'flying'] },
-  {name:'shiftry', height: 1.3, types: ['dark', 'grass'] }];
 
 
-for (let i = 0; i < pokemonList.length, i++) {
-document.write(pokemonList[i].name);
-}
+  let pokemonList = [
+  {name: 'Snorlax', height: 2.1, types: ['normal', 'water'] },
+  {name:'Mewtwo', height: 1.9, types: ['psychic', 'flying'] },
+  {name:'Shiftry', height: 1.3, types: ['dark', 'grass'] }
+];
+
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+   document.write('<br>' + pokemonList[i].name + (' , height: ') + pokemonList[i].height);
+   if (pokemonList[i].height >= 2) {
+     document.write(' - Wow, that\'s big!');
+   }
+ }
