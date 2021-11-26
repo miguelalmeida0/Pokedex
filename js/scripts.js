917c3a806cbdca2ade4009b1175a42d537e9cdc4
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2021-11-26 14:15:32 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
+
   let pokemonList = [{name: 'snorlax', height: 2.1, types: ["normal", "water"]},
   {name:'mewtwo', height: 2, types: ['psychic', 'flying'] },
   {name:'shiftry', height: 1.3, types: ['dark', 'grass'] }];
 
 
-for (let i = 0; i < pokemonList.name, i++) {
+for (let i = 0; i < pokemonList.length, i++) {
 document.write(pokemonList[i].name);
 }
