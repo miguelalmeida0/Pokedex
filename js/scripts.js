f504e2b03ee9871ebffcd707dac8130f46df99ba
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2021-11-30 11:50:58 +0100

    IIFE and forEach Loop Implementation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-
-
-// My initial Pokemons
+// IIFE Implementation
+let pokemonRepository = (function() {
 
   let pokemonList = [
   {name: 'Snorlax', height: 2.1, types: ['normal', 'water'] },
@@ -8,11 +7,28 @@
   {name:'Shiftry', height: 1.3, types: ['dark', 'grass'] }
 ];
 
-// For loop that will run through the Pokemon names, and throw a message related to a Pokemon being bigger than 2 meters.
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
 
-for (let i = 0; i < pokemonList.length; i++) {
+function getAll() {
+  return pokemonList;
+}
+
+return {
+  add: add,
+  getAll: getAll
+};
+
+})();
+// IIFE ends here.
+
+
+// For Each loop that will run through the Pokemon names, and throw a message related to a Pokemon being bigger than 2 meters.
+
+pokemonRepository.getAll().forEach(function(pokemon) {
    document.write('<br>' + pokemonList[i].name + (' , height: ') + pokemonList[i].height);
    if (pokemonList[i].height >= 2) {
      document.write(' - Wow, that\'s big!');
    }
- }
+ });
