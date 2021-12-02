b823dcdc5e5f7cc5f564abd5dd9ff84dc8ae13f8
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2021-12-02 11:16:09 +0100

    Included the For Each loop in the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -2,44 +2,53 @@
 let pokemonRepository = (function (){
 
   let pokemonList = [
-  {name: 'Snorlax', height: 2.1, types: ['normal', 'water']},
-  {name:'Mewtwo', height: 1.9, types: ['psychic', 'flying'] },
-  {name:'Shiftry', height: 1.3, types: ['dark', 'grass'] },
-  {name:'Charmeleon', height: 1.1, types: ['fire', 'normal'] }
-];
+    {name: 'Snorlax', height: 2.1, types: ['normal', 'water']},
+    {name:'Mewtwo', height: 1.9, types: ['psychic', 'flying'] },
+    {name:'Shiftry', height: 1.3, types: ['dark', 'grass'] },
+    {name:'Charmeleon', height: 1.1, types: ['fire', 'normal'] }
+  ];
 
   function add(pokemon) {
-  pokemonList.push(pokemon);
-}
+    pokemonList.push(pokemon);
+  };
 
-function getAll() {
-  return pokemonList;
-}
+  function getAll() {
+    return pokemonList;
+  }
 
-return {
-  add: add,
-  getAll: getAll
-}
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let pokemonItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    // Button Actions
+    button.classList.add('button');
+    pokemonList.appendChild(pokemonItem);
+    pokemonItem.appendChild(button);
 
-function addListItem(pokemon) {
-  let pokemonList = document.querySelector('.pokemon-list');
-  let pokemonItem = document.createElement('li');
-  let button = document.createElement('button');
-  button.innerText = pokemon.name;
-  // Button Actions
-  button.classList.add('button');
-  pokemonList.appendChild(pokemonItem);
-  pokemonItem.appendChild(button);
-}
-  //Events
-  button.addEventListener('click', function () {
-   showDetails(pokemon);
- }
-};
+    //Events
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    });
+  }
 
   function showDetails (pokemon) {
-  console.log(pokemon.name);
-}
+    console.log(pokemon.name);
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+  };
+
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write('<br>' + pokemon.name + (' , height: ') + pokemon.height);
+    if (pokemon.height >= 2) {
+      document.write(' - Wow, that\'s big!');
+    }
+  });
+
 })();
 
 // IIFE ends here.
