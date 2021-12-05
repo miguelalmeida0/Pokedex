c1809bb8791b7e53edb70e411bbd41f4284b642b
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2021-12-05 21:37:23 +0100

    Pokemon List from the API

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,8 @@
 // IIFE Implementation
 let pokemonRepository = (function (){
 
-  let pokemonList = [
-    {name: 'Snorlax', height: 2.1, types: ['normal', 'water']},
-    {name:'Mewtwo', height: 1.9, types: ['psychic', 'flying'] },
-    {name:'Shiftry', height: 1.3, types: ['dark', 'grass'] },
-    {name:'Charmeleon', height: 1.1, types: ['fire', 'normal'] }
-  ];
-
-  function getAll() {
-    return pokemonList;
-  }
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  };
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
@@ -32,20 +20,73 @@ let pokemonRepository = (function (){
     });
   }
 
-  function showDetails (pokemon) {
-    console.log(pokemon.name);
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log()
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
   }
+
+  function loadDetails(item) {
+  let url = item.detailsUrl;
+  return fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (details) {
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = details.types;
+  }).catch(function (e) {
+    console.error(e);
+  });
+}
+
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  };
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
+
+  fetch('https://pokeapi.co/api/v2/pokemon/').then(function (response) {
+  return response.json();
+}).then(function (pokemonList) {
+  console.log(pokemonList);
+}).catch(function () {
+});
 
   return {
     add: add,
+    addListItem: addListItem,
+    loadList: loadList,
     getAll: getAll,
-    addListItem: addListItem
+    loadDetails:loadDetails,
+    showDetails: showDetails,
   };
 })();
 
 // IIFE ends here.
 
 // For Each loop that will run through the Pokemon names, and throw a message related to a Pokemon being bigger than 2 meters.
+pokemonRepository.loadList().then(function() {
 pokemonRepository.getAll().forEach(function(pokemon) {
 pokemonRepository.addListItem(pokemon);
+});
 });
